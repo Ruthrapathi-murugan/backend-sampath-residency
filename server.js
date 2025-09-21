173f8f1979d1c92deb8237d3219bf8c07fcc2c5f
Ruthrapathi-murugan <ruthrapathimurugan@outlook.com>
2025-09-21 22:11:14 +0530

    changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-
 const connectDB = require('./config/db');
 const bookingRoutes = require('./routes/bookingRoutes');
+const cors = require('cors');
 
 const app = express();
 
@@ -10,14 +10,13 @@ const app = express();
 connectDB();
 
 // Middleware
-const cors = require('cors');
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://sampathresidency-palani.netlify.app'],
-    methods: ['GET', 'POST'],
-    credentials: true
-  }));
-  
-  app.options('*', cors());  
+  origin: ['http://localhost:3000', 'https://sampathresidency-palani.netlify.app'],
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
+
+
 
 app.use(express.json());
 
@@ -25,5 +24,4 @@ app.use(express.json());
 app.use('/api/bookings', bookingRoutes);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
