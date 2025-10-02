4235f2c7f2cefce10b4963edf2483107d539af2c
Ruthrapathi-murugan <ruthrapathimurugan@outlook.com>
2025-10-02 20:56:43 +0530

    changes

--- models/Booking.js
+++ models/Booking.js
@@ -9,7 +9,7 @@ const BookingSchema = new mongoose.Schema({
   checkOut: { type: Date, required: true },
   specialRequests: String,
   roomCategory: { type: String, required: true },
-  roomPrice: { type: String, required: true },
+ roomPrice: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -5,4 +5,5 @@ const router = express.Router();
 
 router.post('/', createBooking);
 
+
 module.exports = router;
